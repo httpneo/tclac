64c5cfad5c75b5c03562248abb263d527a1a160a
Stefanfue <stefanfue@gmail.com>
2026-07-08 13:28:16 +0200

    Update tclac.cpp

--- components/tclac/tclac.cpp
+++ components/tclac/tclac.cpp
@@ -253,7 +253,8 @@ void tclacClimate::control(const ClimateCall &call) {
 	
 void tclacClimate::takeControl() {
 	
-	dataTX[7]  = 0b00000000;
+	// dataTX[7]  = 0b00000000;
+	dataTX[7]  = display ? 0b01000000 : 0b00000000;
 	dataTX[8]  = 0b00000000;
 	dataTX[9]  = 0b00000000;
 	dataTX[10] = 0b00000000;
